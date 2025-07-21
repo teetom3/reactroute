825e506af156340cb6304a7b103f77409e6508e1
thomas <thomas.zeryouh@goons.fr>
2025-07-21 11:50:02 +0200

    act8

--- src/pages/Category.jsx
+++ src/pages/Category.jsx
@@ -1,9 +1,20 @@
-import { useParams } from "react-router";
+import { Button } from "react-bootstrap";
+import { useNavigate, useParams } from "react-router";
 const Category = () => {
   const { category } = useParams();
+
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate("/");
+  };
+
   return (
-    <div className="my-5">
+    <div className="my-5 mx-4">
       <h1>{category}</h1>
+      <div className="py-4">
+        <Button onClick={handleRedirect}>Redirection</Button>
+      </div>
     </div>
   );
 };
