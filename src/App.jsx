ada97513299a6331ecf98a108f338d9015ec3de1
thomas <thomas.zeryouh@goons.fr>
2025-07-21 10:08:02 +0200

    act 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,12 @@
 import { Route, Routes } from "react-router";
 import Home from "./pages/Home.jsx";
+import Layout from "./components/Layout.jsx";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      <Route element={<Layout />}>
+        <Route path="/" element={<Home />} />
+      </Route>
     </Routes>
   );
 }
